5da8f10d0c4332af1dedc75d8c00961ce3a29434
rewenset <a.zakurenyi@gmail.com>
2019-03-25 22:13:13 +0200

    add gorilla/mux

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func main() {
+	r := mux.NewRouter().StrictSlash(true)
+
+	r.HandleFunc("/", index)
+	r.HandleFunc("/room/{roomID}", room)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "go-chat")
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
+func room(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "In room %s", vars["roomID"])
 }
